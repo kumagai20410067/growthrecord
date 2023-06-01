dd7acde7923879edabf2ffcdc9479cb75549cc72
kumagai20410067 <zeal20410067@fsgcl.jp>
2023-06-01 14:51:55 +0900

    カレンダーの選択、文字化け対策、ハンバーガーメニューの設置

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-DateTime _focusedDay = DateTime.now();
+DateTime _focused = DateTime.now();
+DateTime? _selected;
+
 void main() {
-  runApp(const MyApp());
+  //runApp(const MyApp());
+initializeDateFormatting('ja').then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GROWTH RECORD',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,fontFamily: 'NotoSansJP'
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GROWTH RECORD'),
     );
   }
 }
@@ -31,19 +35,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
+        endDrawer: Drawer(
+          child: ListView(
+            children: const[
+              DrawerHeader(
+                decoration:BoxDecoration(color:Colors.yellow
+                ),
+                child: Text("My Home Page")),
+                //仮置き
+              ListTile(title: Text("data")),
+              ListTile(title: Text("data2")),
+              ListTile(title: Text("data3"))
+                //仮置き
+            ],
+          ),
+        ),
         body: Center(
           child: TableCalendar(
-            firstDay: DateTime.utc(2022, 4, 1),
+            locale: 'ja_JP',
+            firstDay: DateTime.utc(2023, 4, 1),
             lastDay: DateTime.utc(2025, 12, 31),
-            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selected, day);
+            },
+            onDaySelected: (selected, focused) {
+              if (!isSameDay(_selected, selected)) {
+                setState(() {
+                  _selected = selected;
+                  _focused = focused;
+                });
+              }
+            },
+            focusedDay: _focused,
           ),
         ));
   }
