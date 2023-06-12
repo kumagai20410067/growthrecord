2c28e7c0c863bee4103191cd98ceab2d9120d9db
kumagai20410067 <zeal20410067@fsgcl.jp>
2023-06-12 15:49:11 +0900

    イベントの追加機能作成途中

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'memo.dart';
 
- DateTime _focusedDay = DateTime.now();
-DateTime? _selectedDay;
-CalendarFormat _calendarFormat = CalendarFormat.month;
+
+
+
+
 void main() {
   //runApp(const MyApp());
 initializeDateFormatting('ja').then((_) => runApp(MyApp()));
@@ -37,17 +39,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+   DateTime _focusedDay = DateTime.now();
+   DateTime? _selectedDay;
+   CalendarFormat _calendarFormat = CalendarFormat.month;
+   Map<DateTime, List> _eventsList = {};
+  DateTime? _selected;
+   DateTime _focused = DateTime.now();
+
+//イベントの追加
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+  @override
+  void initState() {
+    super.initState();
+
+    _selected = _focused;
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Test A', 'Test B'],
+      DateTime.now(): ['Test C', 'Test D', 'Test E', 'Test F'],
+    };
+  }
+//イベントの追加
+
   @override
   Widget build(BuildContext context) {
+//イベントの追加
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List getEvent(DateTime day) {
+      return _events[day] ?? [];
+    }
+//イベントの追加
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
+        //ハンバーガーメニュー
         //endDrawerで右側になる
         drawer: Drawer(
           child: ListView(
           children:<Widget>[
-
           Container(
                 child: DrawerHeader(
                   child: Text(
@@ -77,24 +113,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-        ]
- )
-          ),
-            
-          
-          
-
-        
-        body: Center(
-
+        ])),
+          //ハンバーガーメニュー
            
-
-          child: TableCalendar(
+        body: Column(children: [
+          //カレンダーを表示
+           TableCalendar(
             locale: 'ja_JP',
             firstDay: DateTime.utc(2010, 4, 1),
             lastDay: DateTime.utc(2030, 12, 31),
+            eventLoader: getEvent,
             focusedDay: _focusedDay,
 
+            //日付を選択可能に
             selectedDayPredicate: (day) {
               return isSameDay(_selectedDay, day);
             },
@@ -106,17 +137,26 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
               }
             },
-
+            
+            // 「月」「週」変更
             calendarFormat: _calendarFormat,
 
-           onFormatChanged: (format) {  // 「月」「週」変更
+           onFormatChanged: (format) {  
                   if (_calendarFormat != format) {
                     setState(() {
                       _calendarFormat = format;
                     });
                   }
-                }
+                },
           ),
-        ));
+           ListView(
+            shrinkWrap: true,
+            children: getEvent(_selected!)
+                .map((event) => ListTile(
+                      title: Text(event.toString()),
+                    ))
+                .toList(),
+          )
+        ]));
   }
 }
