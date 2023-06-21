4833166606833814f19984fb6736d95e2e251676
kumagai20410067 <zeal20410067@fsgcl.jp>
2023-06-21 11:26:49 +0900

    フォーマットを調整

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:growthrecord/home_page.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-
-
 void main() {
-initializeDateFormatting('ja').then((_) => runApp(MyApp()));
+initializeDateFormatting('ja').then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
