c967c391ca2c108a6483082de029333f0841ea4a
kumagai20410067 <zeal20410067@fsgcl.jp>
2023-06-26 14:14:11 +0900

    不要になったsqliteを削除

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import shared_preferences_foundation
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
