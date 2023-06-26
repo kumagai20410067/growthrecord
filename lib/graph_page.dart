0c2032ede6c68e94c5a6847f747b3667e21da864
kumagai20410067 <zeal20410067@fsgcl.jp>
2023-06-26 22:26:34 +0900

    グラフを仮で表示

--- lib/graph_page.dart
+++ lib/graph_page.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:growthrecord/home_page.dart';
 import 'package:growthrecord/select_page.dart';
@@ -6,21 +7,54 @@ class GraphPage extends StatefulWidget {
 final String selectedPet;
 
 const GraphPage({Key? key, required this.selectedPet}) : super(key: key);
+
 @override
   State<GraphPage> createState() => _GraphPageState();
 }
 
 class _GraphPageState extends State<GraphPage>{
-     int _currentIndex = 2;
+  int _currentIndex = 2;
 
   @override
   void initState() {
     super.initState();
   }
 
+  @override
   Widget build(BuildContext context){
 
     return Scaffold(
+      appBar:AppBar(
+        automaticallyImplyLeading: false,//デフォルトの戻るボタンを削除
+        title: const Text(''),
+        ),
+body: Padding(
+  padding:const EdgeInsets.all(16),
+  child: LineChart(
+          LineChartData(
+            lineBarsData: [
+            LineChartBarData(spots: const [
+              FlSpot(1, 323),
+              FlSpot(2, 538),
+              FlSpot(3, 368),
+              FlSpot(4, 259),
+              FlSpot(5, 551),
+              FlSpot(6, 226),
+              FlSpot(7, 268),
+              FlSpot(8, 296),
+              FlSpot(9, 203),
+              FlSpot(10, 246),
+              FlSpot(11, 345),
+            ])
+          ],
+          titlesData: const FlTitlesData(
+            leftTitles:AxisTitles(sideTitles: SideTitles(showTitles: false)),//左のタイトルを非表示
+            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),//上のタイトルを非表示
+             )
+          ),
+        ),
+   ),
+//フッター
 bottomNavigationBar: BottomNavigationBar(
           items:const [
             BottomNavigationBarItem(
@@ -70,9 +104,3 @@ bottomNavigationBar: BottomNavigationBar(
   }
 
 }
-
-
-
-
-
-
