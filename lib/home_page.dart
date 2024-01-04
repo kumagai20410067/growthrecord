eb684727aebf7d28a79bd4cdcc0b66b3f6067063
kumagai20410067 <zeal20410067@fsgcl.jp>
2024-01-04 14:45:26 +0900

    変更済み

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,19 +15,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late TextEditingController _weightController;
   late TextEditingController _heightController;
+  late TextEditingController _weightController;
   late TextEditingController _memoController;
   late DBHelper.DatabaseHelper _databaseHelper;
   late DateTime _selectedDate;
   bool _showForm = false;
+  bool _dataExists = false; // 新規追加か更新かのフラグ
   List<Map<String, dynamic>> _dailyData = [];
 
   @override
   void initState() {
     super.initState();
-    _weightController = TextEditingController();
     _heightController = TextEditingController();
+    _weightController = TextEditingController();
     _memoController = TextEditingController();
     _databaseHelper = DBHelper.DatabaseHelper.instance;
     _selectedDate = DateTime.now();
@@ -41,6 +42,7 @@ class _HomePageState extends State<HomePage> {
         await _databaseHelper.retrieveData(widget.petId, formattedDate);
     setState(() {
       _dailyData = data;
+      _dataExists = data.isNotEmpty;
     });
   }
 
@@ -63,7 +65,7 @@ class _HomePageState extends State<HomePage> {
             onDaySelected: (selectedDay, focusedDay) {
               setState(() {
                 _selectedDate = selectedDay;
-                _showForm = true;
+                _showForm = false;
                 _fetchDailyData(_selectedDate);
               });
             },
@@ -71,20 +73,26 @@ class _HomePageState extends State<HomePage> {
               return isSameDay(_selectedDate, date);
             },
           ),
+          ElevatedButton(
+            onPressed: () {
+              _showFormAlertDialog(context);
+            },
+            child: Text(_dataExists ? '更新する' : '入力する'),
+          ),
           if (_showForm)
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
                   TextField(
-                    controller: _weightController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(labelText: '体重（kg）'),
-                  ),
-                  TextField(
                     controller: _heightController,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(labelText: '体長（cm）'),
+                  ),
+                  TextField(
+                    controller: _weightController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(labelText: '体重（g）'),
                   ),
                   TextField(
                     controller: _memoController,
@@ -93,6 +101,7 @@ class _HomePageState extends State<HomePage> {
                   ElevatedButton(
                     onPressed: () {
                       _saveData();
+                      Navigator.of(context).pop(); // ダイアログを閉じる
                     },
                     child: Text('保存'),
                   ),
@@ -106,24 +115,22 @@ class _HomePageState extends State<HomePage> {
                 itemBuilder: (context, index) {
                   final data = _dailyData[index];
                   return ListTile(
-                    title: Text(
-                        '体重: ${data['weight']}kg, 体長: ${data['height']}cm, メモ: ${data['memo']}'),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () {
-                            _editData(data);
-                          },
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {
-                            _deleteData(data['id']);
-                          },
-                        ),
+                        Text('体長: ${data['height']}cm'),
+                        Text('体重: ${data['weight']}g'),
+                        Text(
+                            'メモ: ${data['memo'].isEmpty ? 'なし' : data['memo']}'),
+                        if (_dailyData.length > 1 && index > 0) // 差分表示
+                          _buildDifferenceText(data, _dailyData[index - 1]),
                       ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        _showDeleteConfirmationDialog(data['id']);
+                      },
                     ),
                   );
                 },
@@ -156,14 +163,6 @@ class _HomePageState extends State<HomePage> {
               ),
             );
           }
-          // else if (index == 2) {
-          //   Navigator.push(
-          //     context,
-          //     MaterialPageRoute(
-          //       builder: (context) => GraphPage(),
-          //     ),
-          //   );
-          // }
         },
       ),
     );
@@ -172,37 +171,126 @@ class _HomePageState extends State<HomePage> {
   void _saveData() async {
     String formattedDate =
         "${_selectedDate.year}-${_selectedDate.month}-${_selectedDate.day}";
-    double weight = double.tryParse(_weightController.text) ?? 0.0;
     double height = double.tryParse(_heightController.text) ?? 0.0;
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
     String memo = _memoController.text;
 
     int petId = widget.petId;
 
-    await _databaseHelper.insertData(
-        petId, formattedDate, weight, height, memo);
+    if (_dataExists) {
+      // 既存データがある場合は更新
+      await _databaseHelper.updateData(
+          _dailyData[0]['id'], height, weight, memo);
+    } else {
+      // 既存データがない場合は新規追加
+      await _databaseHelper.insertData(
+          petId, formattedDate, height, weight, memo);
+    }
 
-    _weightController.clear();
     _heightController.clear();
+    _weightController.clear();
     _memoController.clear();
 
     setState(() {
-      _showForm = false;
       _fetchDailyData(_selectedDate);
     });
   }
 
-  void _editData(Map<String, dynamic> data) {
-    setState(() {
-      _showForm = true;
-      _selectedDate = DateTime.parse(data['date']);
-      _weightController.text = data['weight'].toString();
-      _heightController.text = data['height'].toString();
-      _memoController.text = data['memo'];
-    });
+  void _showFormAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('データ入力'),
+          contentPadding: EdgeInsets.zero, // 余白を無効にする
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: '体長（cm）'),
+                ),
+                TextField(
+                  controller: _weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: '体重（g）'),
+                ),
+                TextField(
+                  controller: _memoController,
+                  decoration: InputDecoration(labelText: 'メモ'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // ダイアログを閉じる
+              },
+              child: Text('キャンセル'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _saveData();
+                Navigator.of(context).pop(); // ダイアログを閉じる
+              },
+              child: Text('保存'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDeleteConfirmationDialog(int dataId) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('削除の確認'),
+          content: Text('このデータを削除しますか？'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // ダイアログを閉じる
+              },
+              child: Text('キャンセル'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _deleteData(dataId);
+                Navigator.of(context).pop(); // ダイアログを閉じる
+              },
+              child: Text('削除'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _deleteData(int id) async {
     await _databaseHelper.deleteData(id);
     _fetchDailyData(_selectedDate);
+  }
+
+  Widget _buildDifferenceText(
+      Map<String, dynamic> currentData, Map<String, dynamic> previousData) {
+    final heightDifference = currentData['height'] - previousData['height'];
+    final weightDifference = currentData['weight'] - previousData['weight'];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('前回からの差分:'),
+        if (heightDifference != 0)
+          Text('  体長: ${heightDifference > 0 ? '+' : ''}$heightDifference cm'),
+        if (weightDifference != 0)
+          Text('  体重: ${weightDifference > 0 ? '+' : ''}$weightDifference g'),
+      ],
+    );
   }
 }
